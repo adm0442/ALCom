4d6c5e9a6ef1d369b003f136ad5b63af423c31c0
Andreas Lagerkvist <powerbuoy@gmail.com>
2013-08-22 04:28:29 +0200

    Added jQuery Plugins and Projects post types

--- inc/data-objects.php
+++ inc/data-objects.php
@@ -1,15 +1,15 @@
 <?php
-# add_action('init', 'h5b_register_post_types');
+add_action('init', 'h5b_register_post_types');
 
 function h5b_register_post_types () {
-	register_post_type('items', array(
+	register_post_type('projects', array(
 		'labels'			=> array(
-			'name'			=> __('Items', 'h5b'),
-			'singular_label'=> __('Item', 'h5b')
+			'name'			=> __('Projects', 'h5b'),
+			'singular_label'=> __('Project', 'h5b')
 		), 
 		'rewrite'			=> array(
 			'with_front' => false, 
-			'slug' => __('url_items', 'h5b')
+			'slug' => __('url_projects', 'h5b')
 		), 
 		'has_archive'		=> true, 
 		'public'			=> true,
@@ -19,14 +19,14 @@ function h5b_register_post_types () {
 		)
 	));
 
-	register_post_type('locations', array(
+	register_post_type('jquery_plugins', array(
 		'labels'			=> array(
-			'name'			=> __('Locations', 'h5b'),
-			'singular_label'=> __('Location', 'h5b')
+			'name'			=> __('jQuery Plugins', 'h5b'),
+			'singular_label'=> __('jQuery Plugin', 'h5b')
 		), 
 		'rewrite'			=> array(
 			'with_front' => false, 
-			'slug' => __('url_locations', 'h5b')
+			'slug' => __('url_jquery_plugins', 'h5b')
 		), 
 		'has_archive'		=> true, 
 		'public'			=> true,
@@ -35,4 +35,17 @@ function h5b_register_post_types () {
 			'trackbacks', 'custom-fields', 'revisions', 'page-attributes'
 		)
 	));
+
+/*	register_taxonomy('misc', array('items', 'locations'), array(
+		'labels'			=> array(
+			'name'			=> __('Misc', 'h5b'), 
+			'singular_label'=> __('Misc', 'h5b')
+		), 
+		'rewrite'			=> array(
+			'with_front' => false, 
+			'slug' => __('url_misc', 'h5b')
+		), 
+		'sort'				=> true, 
+		'hierarchical'		=> true
+	)); */
 }
