5df333243e4779950fe3f192acce0f4875b3c9b6
Andreas Lagerkvist <powerbuoy@gmail.com>
2013-08-22 05:04:10 +0200

    More work on import

--- inc/import.php
+++ inc/import.php
@@ -13,81 +13,100 @@ function h5b_import () {
 	# Set some stuff
 	$oldDB			= 'ante_alcom';
 	$postType2Table	= array(
-		'post' => 'articles', 
-		'page' => 'pages'
+		'post'				=> 'articles', 
+		'page'				=> 'pages', 
+		'jquery_plugins'	=> 'jquery_plugins'
 	);
-	$postType		= (isset($_GET['post_type']) and array_key_exists($_GET['post_type'], $postType2Table)) ? $_GET['post_type'] : 'post';
 
-	# Switch to old DB and get rows
-	$wpdb->select($oldDB);
+	if (!isset($_GET['post_type']) or !array_key_exists($_GET['post_type'], $postType2Table)) {
+		die('Bad ?post_type');
+	}
 
-	$rows = $wpdb->get_results('SELECT * FROM ' . $postType2Table[$postType]);
+	$postType = $_GET['post_type'];
 
-	foreach ($rows as $row) {
-		# Insert post
-		$wpdb->select(DB_NAME);
+	# jQuery Plugin Files
+	if ($postType == 'jquery_plugins') {
+		foreach (glob(TEMPLATEPATH . '/js/*.js') as $path) {
+			$raw = file_get_contents($path);
 
-		echo "\n # Inserting \"{$row->title}\"";
-
-		$params = array(
-			'post_type'		=> $postType, 
-			'post_title'	=> $row->title, 
-			'post_content'	=> NiceString::makeNice($row->content, 3, false, false, true), 
-			'post_name'		=> $row->url_str, 
-			'post_status'	=> 'publish'
-		);
-
-		if ($postType == 'post') {
-			$params['post_date'] = $row->pub_date;
+			if (strpos($raw, '/***') === 0) {
+				echo $raw;
+			}
 		}
+	}
+	# Database Content
+	else {
+		# Switch to old DB and get rows
+		$wpdb->select($oldDB);
 
-		$newID = wp_insert_post($params);
+		$rows = $wpdb->get_results('SELECT * FROM ' . $postType2Table[$postType]);
 
-		# Insert tags and comments
-		if ($postType == 'post') {
-			# Tags
-			$wpdb->select($oldDB);
-
-			$tmp	= $wpdb->get_results('SELECT * FROM article_tags LEFT JOIN tags USING(tags_id) WHERE articles_id = ' . $row->articles_id);
-			$tags	= array();
-
-			foreach ($tmp as $tag) {
-				$tags[] = $tag->title;
-			}
-
-			if (count($tags)) {
-				$wpdb->select(DB_NAME);
-
-				wp_set_post_terms($newID, $tags, 'post_tag');
-
-				echo " ...with tags: " . implode(', ', $tags);
-			}
-
-			# Comments
-			$wpdb->select($oldDB);
-
-			$comments	= $wpdb->get_results('SELECT * FROM comments WHERE articles_id = ' . $row->articles_id);
-			$tmp		= array();
-
+		foreach ($rows as $row) {
+			# Insert post
 			$wpdb->select(DB_NAME);
 
-			foreach ($comments as $comment) {
-				$tmp[] = substr($comment->content, 0, 15) . '...';
+			echo "\n # Inserting \"{$row->title}\"";
 
-				wp_insert_comment(array(
-					'comment_post_ID'		=> $newID, 
-					'comment_author'		=> $comment->author, 
-					'comment_author_email'	=> $comment->email, 
-					'comment_author_url'	=> $comment->website, 
-					'comment_author_ip'		=> $comment->ip, 
-					'comment_content'		=> NiceString::makeNice($comment->content, 4, false, false, false), 
-					'comment_date'			=> $comment->pub_date, 
-					'comment_approved'		=> 1
-				));
+			$params = array(
+				'post_type'		=> $postType, 
+				'post_title'	=> $row->title, 
+				'post_content'	=> NiceString::makeNice($row->content, 3, false, false, true), 
+				'post_name'		=> $row->url_str, 
+				'post_status'	=> 'publish'
+			);
+
+			if ($postType == 'post') {
+				$params['post_date'] = $row->pub_date;
 			}
 
-			if (count($tmp)) {
-				echo " ... and comments: " . implode(', ', $tmp);
+			$newID = wp_insert_post($params);
+
+			# Insert tags and comments
+			if ($postType == 'post') {
+				# Tags
+				$wpdb->select($oldDB);
+
+				$tmp	= $wpdb->get_results('SELECT * FROM article_tags LEFT JOIN tags USING(tags_id) WHERE articles_id = ' . $row->articles_id);
+				$tags	= array();
+
+				foreach ($tmp as $tag) {
+					$tags[] = $tag->title;
+				}
+
+				if (count($tags)) {
+					$wpdb->select(DB_NAME);
+
+					wp_set_post_terms($newID, $tags, 'post_tag');
+
+					echo " ...with tags: " . implode(', ', $tags);
+				}
+
+				# Comments
+				$wpdb->select($oldDB);
+
+				$comments	= $wpdb->get_results('SELECT * FROM comments WHERE articles_id = ' . $row->articles_id);
+				$tmp		= array();
+
+				$wpdb->select(DB_NAME);
+
+				foreach ($comments as $comment) {
+					$tmp[] = substr($comment->content, 0, 15) . '...';
+
+					wp_insert_comment(array(
+						'comment_post_ID'		=> $newID, 
+						'comment_author'		=> $comment->author, 
+						'comment_author_email'	=> $comment->email, 
+						'comment_author_url'	=> $comment->website, 
+						'comment_author_ip'		=> $comment->ip, 
+						'comment_content'		=> NiceString::makeNice($comment->content, 4, false, false, false), 
+						'comment_date'			=> $comment->pub_date, 
+						'comment_approved'		=> 1
+					));
+				}
+
+				if (count($tmp)) {
+					echo " ... and comments: " . implode(', ', $tmp);
+				}
 			}
 		}
 	}
