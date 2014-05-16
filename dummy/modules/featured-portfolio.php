37ca691085267d7e99e76c7e6faa647133b76fe2
AL <powerbuoy@gmail.com>
2014-05-17 01:53:45 +0200

    Mmmmm I can dig it

--- dummy/modules/featured-portfolio.php
+++ dummy/modules/featured-portfolio.php
@@ -1,59 +1,66 @@
 <section id="featured-portfolio">
 
-	<ul>
-		<li>
-			<ul>
-				<li class="desktop"><img src="img/portfolio/conversionista/desktop.png"></li>
-				<li class="tablet"><img src="img/portfolio/conversionista/tablet.png"></li>
-				<li class="mobile"><img src="img/portfolio/conversionista/mobile.png"></li>
-			</ul>
+	<article>
 
-			<h2><a href="#">Conversionista.com</a></h2>
+		<ul>
+			<li class="desktop"><img src="img/portfolio/conversionista/desktop.png"></li>
+			<li class="tablet"><img src="img/portfolio/conversionista/tablet.png"></li>
+			<li class="mobile"><img src="img/portfolio/conversionista/mobile.png"></li>
+		</ul>
 
-			<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
+		<h2><a href="#">Conversionista.com</a></h2>
 
-			<ul>
-				<li><a href="#">WordPress</a></li>
-				<li><a href="#">Responsive</a></li>
-			</ul>
+		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
 
-			<p><a href="http://conversionista.com">Visit the site</a></p>
-		</li>
-		<li>
-			<ul>
-				<li class="desktop"><img src="img/portfolio/hitta/desktop.png"></li>
-				<li class="tablet"><img src="img/portfolio/hitta/tablet.png"></li>
-				<li class="mobile"><img src="img/portfolio/hitta/mobile.png"></li>
-			</ul>
+		<ul>
+			<li><a href="#">WordPress</a></li>
+			<li><a href="#">Responsive</a></li>
+		</ul>
 
-			<h2><a href="#">Hitta.se</a></h2>
+		<p><a href="http://conversionista.com">Visit the site</a></p>
 
-			<p>A responsive, simple and fresh new version of hitta.se.</p>
+	</article>
 
-			<ul>
-				<li><a href="#">WordPress</a></li>
-				<li><a href="#">Multilingual</a></li>
-			</ul>
+	<article>
 
-			<p><a href="http://beta.hitta.se">Visit the site</a></p>
-		</li>
-		<li>
-			<ul>
-				<li class="desktop"><img src="img/portfolio/sga/desktop.png"></li>
-			</ul>
+		<ul>
+			<li class="desktop"><img src="img/portfolio/hitta/desktop.png"></li>
+			<li class="tablet"><img src="img/portfolio/hitta/tablet.png"></li>
+			<li class="mobile"><img src="img/portfolio/hitta/mobile.png"></li>
+		</ul>
 
-			<h2><a href="#">SafariGuideAfrica.com</a></h2>
+		<h2><a href="#">Hitta.se</a></h2>
 
-			<p>Turning WordPress into a booking site for a travel agent was a fun experience :)</p>
+		<p>A responsive, simple and fresh new version of hitta.se.</p>
 
-			<ul>
-				<li><a href="#">WordPress</a></li>
-				<li><a href="#">Multilingual</a></li>
-			</ul>
+		<ul>
+			<li><a href="#">WordPress</a></li>
+			<li><a href="#">Multilingual</a></li>
+		</ul>
 
-			<p><a href="http://safariguideafrica.com">Visit the site</a></p>
-		</li>
-		<?php # SecMaker, PF, Dubicars mer? ?>
-	</ul>
+		<p><a href="http://beta.hitta.se">Visit the site</a></p>
+
+	</article>
+
+	<article>
+
+		<ul>
+			<li class="desktop"><img src="img/portfolio/sga/desktop.png"></li>
+		</ul>
+
+		<h2><a href="#">SafariGuideAfrica.com</a></h2>
+
+		<p>Turning WordPress into a booking site for a travel agent was a fun experience :)</p>
+
+		<ul>
+			<li><a href="#">WordPress</a></li>
+			<li><a href="#">Multilingual</a></li>
+		</ul>
+
+		<p><a href="http://safariguideafrica.com">Visit the site</a></p>
+
+	</article>
+
+	<?php # SecMaker, PF, Dubicars mer? ?>
 
 </section>
--- dummy/modules/random-projects.php
+++ dummy/modules/random-projects.php
@@ -1,26 +1,29 @@
 <section id="random-projects">
 
-	<ul>
-		<li>
-			<h2>
-				<a href="#"><img src="http://placehold.it/200x200"> HTMLContent</a> 
-				<small><a href="#">WordPress</a></small>
-			</h2>
+	<article>
 
-			<p>Aboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		<h2>
+			<a href="#"><img src="http://placehold.it/200x200"> HTMLContent</a> 
+			<small><a href="#">WordPress</a></small>
+		</h2>
 
-			<p><a href="#">View more</a></p>
-		</li>
-		<li>
-			<h2 class="icon-foo">
-				<a href="#">Image Zoom</a> 
-				<small><a href="#">jQuery</a></small>
-			</h2>
+		<p>Aboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
 
-			<p>Aboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		<p><a href="#">View more</a></p>
 
-			<p><a href="#">View more</a></p>
-		</li>
-	</ul>
+	</article>
+
+	<article>
+
+		<h2 class="icon-foo">
+			<a href="#">Image Zoom</a> 
+			<small><a href="#">jQuery</a></small>
+		</h2>
+
+		<p>Aboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+
+		<p><a href="#">View more</a></p>
+
+	</article>
 
 </section>
