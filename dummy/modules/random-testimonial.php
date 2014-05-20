939faea8189e8eaa76471d75d2fe34a76d110cb0
AL <powerbuoy@gmail.com>
2014-05-20 20:36:59 +0200

    Mhm

--- dummy/modules/random-testimonial.php
+++ dummy/modules/random-testimonial.php
@@ -1,8 +1,8 @@
 <section id="random-testimonial">
 
-	<img src="http://www.dagensps.se/_internal/cimg!0/5pll5ix1u48v6710ftpeu84iqy6dmu3">
+	<img src="http://placehold.it/180x180">
 
-	<h2><cite>Pierre Siri</cite>,<br><b>Entrepreneur</b></h2>
+	<h2><cite>Some CLient</cite>,<br><b>Profession</b></h2>
 
 	<blockquote>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
