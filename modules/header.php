13a2232167740217ab09d1e11a13e56961afcab2
AL <powerbuoy@gmail.com>
2015-03-18 16:56:43 +0100

    Started converting dummy to WP

--- modules/header.php
+++ modules/header.php
@@ -1,28 +1,12 @@
 <header id="header">
 
-	<h1>
+	<?php if (is_front_page()) : ?><h1><?php else : ?><p class="logo"><?php endif ?>
 		<a href="/">
 			Hi! <strong>I'm Andreas</strong> 
 			<small>...and I <em>love</em> building websites.</small>
 		</a>
-	</h1>
+	<?php if (is_front_page()) : ?></h1><?php else : ?></p><?php endif ?>
 
-	<nav>
-		<ul>
-			<li><a href="#">Portfolio</a></li>
-			<li><a href="#">Projects</a></li>
-			<li><a href="#">Blog</a></li>
-			<li><a href="#contact">Hire me!</a></li>
-		</ul>
-	</nav>
-
-	<form method="get" action="" id="search-form">
-
-		<p>
-			<input type="search" name="s" placeholder="Search" required> 
-			<input type="submit" value="Find">
-		</p>
-
-	</form>
+	<?php dynamic_sidebar('header') ?>
 
 </header>
