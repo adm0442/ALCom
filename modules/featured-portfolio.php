3f599b30f442ad4fb61767b1adb7cc15eca7a2e3
AL <powerbuoy@gmail.com>
2015-03-19 23:11:07 +0100

    I cant stop

--- modules/featured-portfolio.php
+++ modules/featured-portfolio.php
@@ -25,76 +25,15 @@
 
 	<article>
 
-		<figure><img src="http://31.192.226.6/alcomimg/aquadental-1920.jpg"></figure>
-		<figure><img src="http://31.192.226.6/alcomimg/aquadental-400.jpg"></figure>
-
-		<h2><a href="#">AquaDental.se</a></h2>
-
-		<ul class="tags">
-			<li><a href="#">wordpress</a></li>
-			<li><a href="#">responsive</a></li>
-		</ul>
-
-		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
-
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
-
-	</article>
-
-	<article>
-
-		<figure><img src="http://31.192.226.6/alcomimg/immensa-1920.jpg"></figure>
-		<figure><img src="http://31.192.226.6/alcomimg/immensa-400.jpg"></figure>
-
-		<h2><a href="#">Immensa.se</a></h2>
-
-		<ul class="tags">
-			<li><a href="#">wordpress</a></li>
-			<li><a href="#">responsive</a></li>
-		</ul>
-
-		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
-
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
-
-	</article>
-
-	<article>
-
-		<figure><img src="http://31.192.226.6/alcomimg/towejewels-1920.jpg"></figure>
-
-		<h2><a href="#">TOWEJewels.com</a></h2>
-
-		<ul class="tags">
-			<li><a href="#">wordpress</a></li>
-			<li><a href="#">responsive</a></li>
-		</ul>
-
-		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
-
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
-
-	</article>
-
-	<article>
-
 		<figure><img src="http://31.192.226.6/alcomimg/bmwfinancialservices-1920.jpg"></figure>
 		<figure><img src="http://31.192.226.6/alcomimg/bmwfinancialservices-400.jpg"></figure>
 
 		<h2><a href="#">BMW Financial Services</a></h2>
 
 		<ul class="tags">
-			<li><a href="#">wordpress</a></li>
-			<li><a href="#">responsive</a></li>
+			<li><a href="#" class="icon-mobile">responsive</a></li>
+			<li><a href="#" class="icon-css3">css3</a></li>
+			<li><a href="#" class="icon-file-code-o">php</a></li>
 		</ul>
 
 		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
@@ -114,30 +53,12 @@
 		<h2><a href="#">DubiCars.com</a></h2>
 
 		<ul class="tags">
-			<li><a href="#">responsive</a></li>
+			<li><a href="#" class="icon-mobile">responsive</a></li>
+			<li><a href="#" class="icon-css3">css3</a></li>
+			<li><a href="#" class="icon-file-code-o">php</a></li>
 		</ul>
 
 		<p>A responsive, simple and fresh new version of exoticcarsdubai.com.</p>
-
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
-
-	</article>
-
-	<article>
-
-		<figure><img src="http://31.192.226.6/alcomimg/secmaker-1920.jpg"></figure>
-
-		<h2><a href="#">SecMaker.com</a></h2>
-
-		<ul class="tags">
-			<li><a href="#">wordpress</a></li>
-			<li><a href="#">multilingual</a></li>
-		</ul>
-
-		<p>Turning WordPress into a booking site for a travel agent was a fun experience :)</p>
 
 		<p>
 			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
@@ -154,8 +75,9 @@
 		<h2><a href="#">Conversionista.com</a></h2>
 
 		<ul class="tags">
-			<li><a href="#">wordpress</a></li>
-			<li><a href="#">responsive</a></li>
+			<li><a href="#" class="icon-wordpress">wordpress-theme</a></li>
+			<li><a href="#" class="icon-mobile">responsive</a></li>
+			<li><a href="#" class="icon-css3">css3</a></li>
 		</ul>
 
 		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
