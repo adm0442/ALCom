0269b56b3171fd3db23e027d4f2c59333ba5043c
AL <powerbuoy@gmail.com>
2015-03-19 22:44:11 +0100

    nicer

--- modules/random-projects.php
+++ modules/random-projects.php
@@ -3,8 +3,8 @@
 	<article>
 
 		<h2>
-			<a href="#" class="icon-html5">HTMLWidget</a> 
-			<small><a href="#">WordPress</a></small>
+			<small><a href="#">WordPress</a></small> 
+			<a href="#" class="icon-html5">HTMLWidget</a>
 		</h2>
 
 		<ul class="tags">
@@ -21,8 +21,8 @@
 	<article>
 
 		<h2>
-			<a href="#" class="icon-search-plus">Image Zoom</a> 
-			<small><a href="#">jQuery</a></small>
+			<small><a href="#">jQuery</a></small> 
+			<a href="#" class="icon-search-plus">Image Zoom</a>
 		</h2>
 
 		<ul class="tags">
