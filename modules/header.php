e7167cf17393fe6faabccaa6d28bad11297083c6
AL <powerbuoy@gmail.com>
2015-03-20 22:57:09 +0100

    Oh yea

--- modules/header.php
+++ modules/header.php
@@ -1,9 +1,11 @@
+<?php $verbs = array('love', 'live for', 'obsess over', 'lose sleep over', 'dream about') ?>
+
 <header id="header">
 
 	<?php if (is_front_page()) : ?><h1><?php else : ?><p class="logo"><?php endif ?>
 		<a href="/">
 			Hi! <strong>I'm Andreas</strong> 
-			<small>...and I <em>love</em> building websites.</small>
+			<small>...and I <em><?php echo $verbs[array_rand($verbs, 1)] ?></em> web development</small>
 		</a>
 	<?php if (is_front_page()) : ?></h1><?php else : ?></p><?php endif ?>
 
