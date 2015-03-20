296118327d7f1098264cce2809de532f65e070ef
AL <powerbuoy@gmail.com>
2015-03-20 16:36:57 +0100

    More work on header + moved featured-portfolio into main where it belongs

--- front-page.php
+++ front-page.php
@@ -1,9 +1,8 @@
 <?php get_header() ?>
 
-<?php include get_stylesheet_directory() . '/modules/featured-portfolio.php' ?>
-
 <main>
 
+	<?php include get_stylesheet_directory() . '/modules/featured-portfolio.php' ?>
 	<?php include get_stylesheet_directory() . '/modules/random-testimonials.php' ?>
 	<?php include get_stylesheet_directory() . '/modules/latest-blog.php' ?>
 	<?php include get_stylesheet_directory() . '/modules/random-projects.php' ?>
