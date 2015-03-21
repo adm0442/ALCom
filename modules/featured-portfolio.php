871cbb8dd4192108d90d1b6d8044d137ada90edf
AL <powerbuoy@gmail.com>
2015-03-21 15:53:19 +0100

    Portfolio real cod

--- modules/featured-portfolio.php
+++ modules/featured-portfolio.php
@@ -1,94 +1,43 @@
+<?php
+	global $post;
+
+	$rows = get_posts(array(
+		'post_type' => 'portfolio', 
+		'numberposts' => -1, 
+		'orderby' => 'date', 
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'misc', 
+				'field' => 'slug', 
+				'terms' => 'featured'
+			)
+		)
+	));
+?>
+
 <section id="featured-portfolio">
 
-	<article>
+	<?php foreach ($rows as $post) : setup_postdata($post) ?>
+		<article>
 
-		<figure><img src="http://31.192.226.6/alcomimg/exoticcars-1920.jpg"></figure>
-		<figure><img src="http://31.192.226.6/alcomimg/exoticcars-400.jpg"></figure>
+			<figure><img src="<?php the_field('desktop_screenshot') ?>"></figure>
 
-		<h2><a href="#">ExoticCarsDubai.com</a></h2>
+			<?php if ($mobileScreenshot = get_field('mobile_screenshot')) : ?>
+				<figure><img src="<?php the_field('mobile_screenshot') ?>"></figure>
+			<?php endif ?>
 
-		<ul class="tags">
-			<li><a href="#" class="icon-wordpress">wordpress</a></li>
-			<li><a href="#" class="icon-mobile">responsive</a></li>
-			<li><a href="#" class="icon-css3">css3</a></li>
-			<li><a href="#">jquery</a></li>
-		</ul>
+			<h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
 
-		<p>A responsive, simple and fresh new version of exoticcarsdubai.com.</p>
+			<?php sleek_get_module('partials/tags', array('taxonomy' => 'portfolio_tags')) ?>
 
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
+			<?php the_excerpt() ?>
 
-	</article>
+			<p>
+				<a href="<?php the_field('site_url') ?>" class="button">Visit the site</a> or 
+				<a href="<?php the_permalink() ?>" class="button secondary">Read more about the project</a>
+			</p>
 
-	<article>
-
-		<figure><img src="http://31.192.226.6/alcomimg/bmwfinancialservices-1920.jpg"></figure>
-		<figure><img src="http://31.192.226.6/alcomimg/bmwfinancialservices-400.jpg"></figure>
-
-		<h2><a href="#">BMW Financial Services</a></h2>
-
-		<ul class="tags">
-			<li><a href="#" class="icon-mobile">responsive</a></li>
-			<li><a href="#" class="icon-css3">css3</a></li>
-			<li><a href="#" class="icon-file-code-o">php</a></li>
-		</ul>
-
-		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
-
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
-
-	</article>
-
-	<article>
-
-		<figure><img src="http://31.192.226.6/alcomimg/dubicars-1920.jpg"></figure>
-		<figure><img src="http://31.192.226.6/alcomimg/dubicars-400.jpg"></figure>
-
-		<h2><a href="#">DubiCars.com</a></h2>
-
-		<ul class="tags">
-			<li><a href="#" class="icon-mobile">responsive</a></li>
-			<li><a href="#" class="icon-css3">css3</a></li>
-			<li><a href="#" class="icon-file-code-o">php</a></li>
-		</ul>
-
-		<p>A responsive, simple and fresh new version of exoticcarsdubai.com.</p>
-
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
-
-	</article>
-
-	<article>
-
-		<figure><img src="http://31.192.226.6/alcomimg/conversionista-1920.jpg"></figure>
-		<figure><img src="http://31.192.226.6/alcomimg/conversionista-400.jpg"></figure>
-
-		<h2><a href="#">Conversionista.com</a></h2>
-
-		<ul class="tags">
-			<li><a href="#" class="icon-wordpress">wordpress</a></li>
-			<li><a href="#" class="icon-mobile">responsive</a></li>
-			<li><a href="#" class="icon-css3">css3</a></li>
-		</ul>
-
-		<p>Fully responsive and fully awesome, building from mobile up is the way to go!</p>
-
-		<p>
-			<a href="http://www.exoticcarsdubai.com" class="button">Visit the site</a> or 
-			<a href="#" class="button secondary">Read more about the project</a>
-		</p>
-
-	</article>
-
-	<?php # PF, Immensa?, SGA? mer? ?>
+		</article>
+	<?php endforeach; wp_reset_postdata() ?>
 
 </section>
