ee7e6a117c0a038885a5faec40897d358089f1b5
AL <powerbuoy@gmail.com>
2015-03-21 22:04:01 +0100

    Header :D :D :D

--- modules/header.php
+++ modules/header.php
@@ -1,5 +1,5 @@
 <?php
-	# Make certain verbs more likely to appear
+	# Make certain words more likely to appear
 	$verbs = array(
 		'love', 'love', 'love', 'love', 
 		'live for', 'live for', 'live for', 'live for', 
@@ -8,6 +8,19 @@
 		'dream about', 'dream about', 'dream about', 'dream about', 'dream about', 'dream about', 'dream about', 
 		'sometimes feel like cuddling with'
 	);
+
+	$things = array(
+		'web development', 'web development', 'web development', 
+		'video games', 
+		'gadgets', 
+		'open source', 'open source', 'open source', 
+		'ubuntu', 'ubuntu', 'ubuntu'
+	);
+
+	if (is_front_page()) {
+		$verbs = array('build websites');
+		$things = array('for people');
+	}
 ?>
 
 <header id="header">
@@ -15,7 +28,7 @@
 	<?php if (is_front_page()) : ?><h1><?php else : ?><p class="logo"><?php endif ?>
 		<a href="<?php echo home_url('/') ?>">
 			Hi! <strong>I'm Andreas</strong> 
-			<small>...and I <em><?php echo $verbs[array_rand($verbs, 1)] ?></em> web development</small>
+			<small>...and I <em><?php echo $verbs[array_rand($verbs, 1)] ?></em> <?php echo $things[array_rand($things, 1)] ?></small>
 		</a>
 	<?php if (is_front_page()) : ?></h1><?php else : ?></p><?php endif ?>
 
