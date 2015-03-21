4d6eb7681e17c77c19f25ae40b7daae932f124a9
AL <powerbuoy@gmail.com>
2015-03-21 15:24:53 +0100

    Started styling the post page

---
+++ modules/partials/post-pubdate.php
@@ -0,0 +1,1 @@
+<p><time datetime="<?php the_time('Y-m-j') ?>" pubdate="pubdate"><?php echo get_the_time(get_option('date_format')) ?></time></p>
---
+++ modules/post.php
@@ -0,0 +1,27 @@
+<section id="post">
+
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<header>
+
+			<?php the_post_thumbnail('sleek-medium') ?>
+
+			<h1><?php the_title() ?></h1>
+
+			<?php sleek_get_module('partials/tags', array('taxonomy' => 'post_tag')) ?>
+			<?php sleek_get_module('partials/post-pubdate') ?>
+
+		</header>
+
+		<div>
+			<?php the_content() ?>
+		</div>
+
+		<nav id="pagination">
+			<?php previous_post_link('<span class="prev">%link</span>', '%title') ?>
+			<?php next_post_link('<span class="next">%link</span>', '%title') ?>
+		</nav>
+	<?php endwhile; else : ?>
+		<?php sleek_get_module('partials/nothing-found') ?>
+	<?php endif ?>
+
+</section>
