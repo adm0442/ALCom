f48a99a5655259ebbdbf5d2a974d33449ecb4c78
AL <powerbuoy@gmail.com>
2015-03-23 21:38:52 +0100

    hero header texture + loads of other fixes

--- modules/posts-intro.php
+++ modules/posts-intro.php
@@ -7,7 +7,7 @@
 <?php if ($title or $content) : ?>
 	<header id="posts-intro">
 
-		<img src="<?php $ud = wp_upload_dir(); echo $ud['baseurl'] ?>/2015/03/typewriter-1920x800.jpg">
+		<!--<img src="<?php $ud = wp_upload_dir(); echo $ud['baseurl'] ?>/2015/03/typewriter-1920x800.jpg">-->
 
 		<?php if ($title) : ?>
 			<h1><?php echo $title ?></h1>
