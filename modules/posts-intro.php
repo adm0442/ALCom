c17a5d0f3db6972a322f3d3d7532402773b72523
AL <powerbuoy@gmail.com>
2015-03-24 15:53:11 +0100

    SHow tags in listings

--- modules/posts-intro.php
+++ modules/posts-intro.php
@@ -13,6 +13,14 @@
 			<h1><?php echo $title ?></h1>
 		<?php endif ?>
 
+		<?php if (is_post_type_archive('portfolio')) : ?>
+			<?php sleek_get_module('partials/all-tags', array('taxonomy' => 'portfolio_tags')) ?>
+		<?php elseif (is_post_type_archive('projects')) : ?>
+			<?php sleek_get_module('partials/all-tags', array('taxonomy' => 'project_tags')) ?>
+		<?php elseif (is_home()) : ?>
+			<?php sleek_get_module('partials/all-tags', array('taxonomy' => 'post_tag')) ?>
+		<?php endif ?>
+
 		<?php if ($content) : ?>
 			<?php echo $content ?>
 		<?php endif ?>
