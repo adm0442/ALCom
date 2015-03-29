2bb9825f900e5522d461c5e301ac370c27ad676c
AL <powerbuoy@gmail.com>
2015-03-29 15:10:28 +0200

    Use file.version.css instead of ?version

--- functions.php
+++ functions.php
@@ -12,16 +12,16 @@ function alcom_register_css_js () {
 	# No need for jQuery 2015 :D
 	wp_deregister_script('jquery');
 
-	# Google Maps
-#	wp_register_script('google_maps', 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false');
-#	wp_enqueue_script('google_maps');
-
 	# Theme JS
-#	wp_register_script('alcom_foot', get_stylesheet_directory_uri() . '/js/foot.php', array(), filemtime(get_stylesheet_directory() . '/js/foot.js'), true);
+#	wp_register_script('alcom_foot', get_styleheet_directory_uri() . '/js/foot.php', array(), null, true);
+#	wp_register_script('alcom_foot', get_styleheet_directory_uri() . '/js/foot.' . filemtime(get_stylesheet_directory() . '/js/head.js') . '.js', array(), null, true);
 #	wp_enqueue_script('alcom_foot');
 
 	# Theme CSS
-	wp_register_style('alcom', get_stylesheet_directory_uri() . '/css/all.css', array(), filemtime(get_stylesheet_directory() . '/css/all.css'));
+#	wp_register_style('alcom', get_stylesheet_directory_uri() . '/css/all.css', array(), filemtime(get_stylesheet_directory() . '/css/all.css'));
+#	wp_enqueue_style('alcom');
+
+	wp_register_style('alcom', get_stylesheet_directory_uri() . '/css/all.' . filemtime(get_stylesheet_directory() . '/css/all.css') . '.css', array(), null);
 	wp_enqueue_style('alcom');
 }
 
