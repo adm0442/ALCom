5a582d0c7f471747eef0d95218f49ef393e9c1d7
AL <powerbuoy@gmail.com>
2015-03-29 20:56:24 +0200

    Use RequestAnimationFrame instead :D

--- js/foot-2-trippy-bg.js
+++ js/foot-2-trippy-bg.js
@@ -47,12 +47,19 @@ var TrippyBG = {
 		var waveHeight = self.dim.height / 20;
 		var offset = self.dim.height - waveHeight * 2;
 		var direction = -1;
+		var time = new Date().getTime();
+		var dt = 0;
+		var i = 0;
 
 		// Start drawing
 		self.ctx.lineWidth = 1;
 
-		var drawWaves = function (t) {
-			offset += .1 * direction;
+		var drawWaves = function () {
+			dt = (new Date().getTime() - time) / 1000;
+			time = new Date().getTime();
+
+			offset += (1 * dt) * direction;
+			i += (100 * dt);
 
 			if (offset < (0 + waveHeight)) {
 				direction = 1;
@@ -65,7 +72,7 @@ var TrippyBG = {
 			self.ctx.moveTo(0, self.dim.height / 2 + offset);
 
 			for (var x = 0; x < self.dim.width; x++) {
-				var yPos = waveHeight * (Math.sin((t + x) * Math.PI / 180) * ((Math.sin(((x * 1.5)) * Math.PI / 180) + 1) / 1.5));
+				var yPos = waveHeight * (Math.sin((i + x) * Math.PI / 180) * ((Math.sin(((x * 1.5)) * Math.PI / 180) + 1) / 1.5));
 					yPos += offset;
 
 				self.ctx.beginPath();
@@ -82,12 +89,14 @@ var TrippyBG = {
 				self.ctx.closePath();
 				self.ctx.stroke();
 			}
+
+			var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
+
+			raf(drawWaves);
 		};
 
-		var t = 0;
+		var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
 
-		setInterval(function () {
-			drawWaves(t += 4);
-		}, 50);
+		raf(drawWaves);
 	}
 };
