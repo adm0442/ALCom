13dc4782efb53b843e4b1827cfd11f803c8f95b7
AL <powerbuoy@gmail.com>
2015-04-05 20:00:01 +0200

    More physics

--- js/foot-2-trippy-bg.js
+++ js/foot-2-trippy-bg.js
@@ -45,6 +45,8 @@ var TrippyBG = {
 
 	// Box2d
 	physics: function () {
+		var self = this;
+
 		this.canvas.style.opacity = .2;
 
 		// Shortcuts
@@ -67,7 +69,7 @@ var TrippyBG = {
 		var canvasH = this.dim.height;
 
 		// Box2d vars
-		var world = new b2World(new b2Vec2(0, gravity), true);;
+		var world = new b2World(new b2Vec2(0, gravity), true);
 
 		// Debug draw
 		var debugDraw = new b2DebugDraw();
@@ -80,11 +82,10 @@ var TrippyBG = {
 
 		world.SetDebugDraw(debugDraw);
 
-		// Creates box
-		var createBox = function (x, y, w, h, r, type) {
+		// Create box or ball
+		var createObject = function (x, y, r, shape, type) {
 			var bodyDef = new b2BodyDef;
 			var fixtureDef = new b2FixtureDef;
-			var shape = new b2PolygonShape;
 
 			bodyDef.type = typeof(type) != 'undefined' ? type : b2Body.b2_dynamicBody;
 
@@ -93,8 +94,6 @@ var TrippyBG = {
 			bodyDef.position.y = y;
 
 			// Random sized box
-			shape.SetAsBox(w, h);
-
 			fixtureDef.shape = shape;
 
 			// Tweaking
@@ -119,63 +118,88 @@ var TrippyBG = {
 			};
 		};
 
-		// Creates ball
-		var createBall = function (x, y, s, r, type) {
-			var bodyDef = new b2BodyDef;
-			var fixtureDef = new b2FixtureDef;
+		// Creates a box
+		var createBox = function (x, y, w, h, r, type) {
 			var shape = new b2PolygonShape;
+			shape.SetAsBox(w, h);
 
-			bodyDef.type = typeof(type) != 'undefined' ? type : b2Body.b2_dynamicBody;
+			return createObject(x, y, r, shape, type);
+		};
 
-			// Position in the middle - above screen
-			bodyDef.position.x = x;
-			bodyDef.position.y = y;
-
-			// Random sized box
-			fixtureDef.shape = new b2CircleShape(s);
-
-			// Tweaking
-			fixtureDef.density = 4;
-			fixtureDef.friction = 0.5;
-			fixtureDef.restitution = 0.1;
-
-			// Create
-			var body = world.CreateBody(bodyDef);
-			var fixture = body.CreateFixture(fixtureDef);
-
-			body.SetLinearDamping(0.4);
-
-			// Spin
-			if (r) {
-				body.SetAngularVelocity(r);
-			}
-
-			return {
-				bodyDef: bodyDef, 
-				fixtureDef: fixtureDef, 
-				body: body, 
-				fixture: fixture
-			};
+		// Creates a ball
+		var createBall = function (x, y, s, r, type) {
+			return createObject(x, y, r, new b2CircleShape(s), type);
 		};
 
 		// Create ground
 		var ground = createBox(0, canvasH / pxPerM + 1, (canvasW / pxPerM) * 3, 1, 0, b2Body.b2_staticBody);
+		var groundLevel = canvasH / pxPerM;
 
-		// Create boxes
-		var objects = [];
-		var maxW = 1.5;
-		var maxH = 1.5;
+		// Build a pyramid of boxes
+		var buildPyramid = function (baseNum, size, offsetX, offsetY) {
+			var baseNum = baseNum || 5;
+			var size = size || 1;
+			var boxes = [];
 
-		objects.push(createBox((canvasW / pxPerM) / 2 + (Math.random() * 4 - 2), -10, Math.random() * maxW + 0.5, Math.random() * maxH + 0.5, Math.random() * 20 - 10));
+			for (var row = 0; row < baseNum; row++) {
+				for (var col = 0; col < (baseNum - row); col++) {
+					var x = (col * size + size) + (row * (size / 2)) + offsetX;
+					var y = -((row * size) - offsetY);
 
-		setInterval(function () {
-			if (Math.round(Math.random() * 2) == 2) {
-				objects.push(createBall((canvasW / pxPerM) / 2 + (Math.random() * 4 - 2), -5, Math.random() * maxW + 0.5, Math.random() * 20 - 10));
+					boxes.push(createBox(x, y, size / 2, size / 2, 0, b2Body.b2_dynamicBody));
+				}
 			}
-			else {
-				objects.push(createBox((canvasW / pxPerM) / 2 + (Math.random() * 4 - 2), -5, Math.random() * maxW + 0.5, Math.random() * maxH + 0.5, Math.random() * 20 - 10));
-			}
-		}, 5000);
+
+			return boxes;
+		};
+
+		// Bowling
+		if (Math.round(Math.random())) {
+			var boxes = buildPyramid(10, 1, ((canvasW / pxPerM) / 2) - 5, groundLevel - 1);
+			var balls = [];
+
+			setInterval(function () {
+				var newBall = createBall(-5, Math.random() * groundLevel - 1, 1, 0, b2Body.b2_dynamicBody);
+
+				balls.push(newBall);
+
+				newBall.body.ApplyImpulse(new b2Vec2(1000, 0), newBall.body.GetWorldCenter());
+			}, 5000);
+		}
+		// Create boxes every 5 seconds
+		else {
+			var objects = [];
+			var maxW = 1.5;
+			var maxH = 1.5;
+
+			objects.push(createBox(
+				(canvasW / pxPerM) / 2 + (Math.random() * 4 - 2), 
+				-10, 
+				Math.random() * maxW + 0.5, 
+				Math.random() * maxH + 0.5, 
+				Math.random() * 20 - 10
+			));
+
+			setInterval(function () {
+				if (Math.round(Math.random() * 2) == 2) {
+					objects.push(createBall(
+						(canvasW / pxPerM) / 2 + (Math.random() * 4 - 2), 
+						-5, 
+						Math.random() * maxW + 0.5, 
+						Math.random() * 20 - 10
+					));
+				}
+				else {
+					objects.push(createBox(
+						(canvasW / pxPerM) / 2 + (Math.random() * 4 - 2), 
+						-5, 
+						Math.random() * maxW + 0.5, 
+						Math.random() * maxH + 0.5, 
+						Math.random() * 20 - 10
+					));
+				}
+			}, 5000);
+		}
 
 		// Update
 		var update = function () {
