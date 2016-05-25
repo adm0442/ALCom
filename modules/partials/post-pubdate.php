458586b2ac48a46cd74a9a1c05d099e326e6c1fa
AL <powerbuoy@gmail.com>
2016-05-26 00:44:07 +0200

    Loads :D

--- modules/partials/post-pubdate.php
+++ modules/partials/post-pubdate.php
@@ -1,11 +1,15 @@
 <p class="pubdate">
-	<?php printf(__('Posted %s by %s', 'sleek'),
-		'<time class="icon-clock" datetime="' . get_the_time('Y-m-j') . '">' . get_the_time(get_option('date_format')) . '</time>', 
-		'<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('display_name') . '</a>'
-	) ?><?php if (has_category()) : ?>, <?php printf(__('filed under %s', 'sleek'), get_the_category_list(', ')) ?><?php endif ?>.
-</p>
+	<small class="icon-clock">
+		<?php printf(__('Posted %s', 'sleek'), '<time datetime="' . get_the_time('Y-m-j') . '">' . get_the_time(get_option('date_format')) . '</time>') ?>
+	</small>
 
-<?php /* <p>
-	<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>"><?php echo get_the_author_meta('display_name') ?></a>
-	<time><?php echo get_the_time(get_option('date_format')) ?></time>
-</p> */ ?>
+	<small class="icon-user">
+		<?php printf(__('By %s', 'sleek'), '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('display_name') . '</a>') ?>
+	</small>
+
+	<?php if (has_category()) : ?>
+		<small class="icon-archive">
+			<?php printf(__('Filed under %s', 'sleek'), get_the_category_list(', ')) ?>
+		</small>
+	<?php endif ?>
+</p>
