ce0171c772f14b02fb178566e6676bf9a352e73e
AL <powerbuoy@gmail.com>
2016-05-26 01:49:07 +0200

    Moar and moar

--- modules/partials/post-pubdate.php
+++ modules/partials/post-pubdate.php
@@ -1,15 +1,15 @@
 <p class="pubdate">
-	<small class="icon-clock">
+	<span class="icon-clock">
 		<?php printf(__('Posted %s', 'sleek'), '<time datetime="' . get_the_time('Y-m-j') . '">' . get_the_time(get_option('date_format')) . '</time>') ?>
-	</small>
+	</span>
 
-	<small class="icon-user">
+	<span class="icon-user">
 		<?php printf(__('By %s', 'sleek'), '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('display_name') . '</a>') ?>
-	</small>
+	</span>
 
 	<?php if (has_category()) : ?>
-		<small class="icon-archive">
+		<span class="icon-archive">
 			<?php printf(__('Filed under %s', 'sleek'), get_the_category_list(', ')) ?>
-		</small>
+		</span>
 	<?php endif ?>
 </p>
