169d17d56e472830e6286c71d453c74bdd3cb175
AL <powerbuoy@gmail.com>
2016-09-27 02:40:41 +0200

    Cleanup

--- functions.php
+++ functions.php
@@ -219,76 +219,69 @@ add_filter('user_contactmethods', function () {
  */
 add_action('after_setup_theme', function () {
 	load_child_theme_textdomain('alcom', get_stylesheet_directory() . '/languages');
+
+	# If you want to override parent theme translations, add them to languages/sleek/lang_Code.po
+	# load_theme_textdomain('sleek', get_stylesheet_directory() . '/languages/sleek');
 });
 
 /**
  * These are optional actions to improve how WP normally does things
  */
- /**
-  * These are optional hacks to improve how WP normally does things
-  */
- # Remove WPMU signup stylesheet
- /* add_action('get_header', function () {
- 	remove_action('wp_head', 'wpmu_signup_stylesheet');
- }); */
 
- # Give pages excerpts
- # add_action('init', 'sleek_add_excerpts_to_pages');
+# Remove WPMU signup stylesheet
+/* add_action('get_header', function () {
+	remove_action('wp_head', 'wpmu_signup_stylesheet');
+}); */
 
- # Disable WP Embed
- add_action('wp_enqueue_scripts', 'sleek_disable_wp_embed');
+# Give pages excerpts
+/* add_action('init', function () {
+	add_post_type_support('page', 'excerpt');
+}); */
 
- # Adds a Browser Update script for older browsers
- # add_action('wp_head', 'sleek_register_browser_update_js');
+# Disable WP Embed
+add_action('wp_enqueue_scripts', 'sleek_disable_wp_embed');
 
- # Allow shortcodes in Widgets
- # add_action('init', 'sleek_allow_shortcodes_in_widgets');
+# Allow shortcodes in Widgets
+/* add_action('init', function () {
+	add_filter('widget_text', 'do_shortcode');
+}); */
 
- # Cleanup HEAD
- add_action('init', 'sleek_cleanup_head');
+# Move jQuery to bottom of page + include from CDN
+add_action('wp_enqueue_scripts', 'sleek_enqueue_jquery_cdn_in_footer');
 
- # Move jQuery to bottom of page + include from CDN
- add_action('wp_enqueue_scripts', 'sleek_enqueue_jquery_cdn_in_footer');
+# Allow Markdown in excerpts and ACF
+add_action('init', 'sleek_more_markdown');
 
- # Allow Markdown in excerpts and ACF
- add_action('init', 'sleek_more_markdown');
+# Add open graph tags to posts (unless Yoast SEO is in use)
+# add_action('wp_head', 'sleek_open_graph_tags');
 
- # Add open graph tags to posts (unless Yoast SEO is in use)
- # add_action('wp_head', 'sleek_open_graph_tags');
+# Remove Emoji CSS/JS from head added since WP 4.2.2
+add_action('init', 'sleek_remove_emoji_css_js');
 
- # Remove Emoji CSS/JS from head added since WP 4.2.2
- add_action('init', 'sleek_remove_emoji_css_js');
+# Disable CF7 CSS and/or JS
+# add_filter('wpcf7_load_js', '__return_false');
+add_filter('wpcf7_load_css', '__return_false');
 
- # Disable CF7 CSS and/or JS
- # add_filter('wpcf7_load_js', '__return_false');
- add_filter('wpcf7_load_css', '__return_false');
+# Add an "active-parent" class to archive pages when browsing their taxonomies
+add_filter('nav_menu_css_class', 'sleek_active_archive_link_on_taxonomies', 10, 2);
 
- # Add an "active-parent" class to archive pages when browsing their taxonomies
- add_filter('nav_menu_css_class', 'sleek_active_archive_link_on_taxonomies', 10, 2);
+# Allow a 'post_type' => [] argument in get_terms()
+add_filter('terms_clauses', 'sleek_terms_clauses', 10, 3);
 
- # Allow a 'post_type' => [] argument in get_terms()
- add_filter('terms_clauses', 'sleek_terms_clauses', 10, 3);
+# Add placeholders to comment form
+add_filter('comment_form_defaults', 'sleek_comment_form_placeholders');
 
- # Add placeholders to comment form
- add_filter('comment_form_defaults', 'sleek_comment_form_placeholders');
-
- # Disable Ultimate Post Widget CSS
+# Disable Ultimate Post Widget CSS
 # add_filter('upw_enqueue_styles', '__return_false');
 
- # Excludes the currently viewed post in UPW
- # add_filter('upw_wp_query_args', 'sleek_exclude_current_post_in_upw');
+# Excludes the currently viewed post in UPW
+# add_filter('upw_wp_query_args', 'sleek_exclude_current_post_in_upw');
 
- # Allow HTML in Widget Titles (with [tags])
- # add_filter('widget_title', 'sleek_html_in_widget_titles');
+# Allow HTML in Widget Titles (with [tags])
+# add_filter('widget_title', 'sleek_html_in_widget_titles');
 
- # Remove HOME from Yoast Breadcrumbs
- # add_filter('wpseo_breadcrumb_links', 'sleek_remove_home_from_breadcrumb');
+# Remove HOME from Yoast Breadcrumbs
+# add_filter('wpseo_breadcrumb_links', 'sleek_remove_home_from_breadcrumb');
 
- # Show all posts (ignore posts_per_page setting) when browsing custom post types
- # add_filter('pre_get_posts', 'sleek_show_all_cpt_posts');
-
- # Show all post types when browsing author
- # add_filter('pre_get_posts', 'sleek_show_all_post_types_for_authors');
-
- # Remove .current_page_parent from Blog-page when viewing another archive
- add_filter('nav_menu_css_class', 'sleek_unset_active_blog_class', 10, 2);
+# Remove .current_page_parent from Blog-page when viewing another archive
+add_filter('nav_menu_css_class', 'sleek_unset_active_blog_class', 10, 2);
